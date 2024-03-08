19938e42a7b267db07a78f07371b28ff52a74f4c
Anthony Bussani <bussania@gmail.com>
2024-03-08 15:57:14 +0100

    change training sequence, first win and lost and finally drawn to keep value around 0.0

--- src/main/java/com/aquila/chess/fit/AbstractFit.java
+++ src/main/java/com/aquila/chess/fit/AbstractFit.java
@@ -1,5 +1,6 @@
 package com.aquila.chess.fit;
 
+import com.aquila.chess.TrainGame;
 import com.aquila.chess.strategy.mcts.StatisticsFit;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
@@ -13,7 +14,6 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
-import java.util.stream.Collectors;
 
 @Slf4j
 public class AbstractFit {
@@ -24,6 +24,10 @@ public class AbstractFit {
     @Getter
     ConfigFit configFit;
 
+    SortedMap<Long, File> drawGames = new TreeMap<>();
+    SortedMap<Long, File> winGames = new TreeMap<>();
+    SortedMap<Long, File> lostGames = new TreeMap<>();
+
     public AbstractFit(String configFile) throws JAXBException {
         JAXBContext context = JAXBContext.newInstance(ConfigFit.class);
         file = new File(configFile);
@@ -31,14 +35,29 @@ public class AbstractFit {
     }
 
     public void run(TrainFile trainFile, Map<String, StatisticsFit> statistics) {
-        Map<Long, File> files = retrieveAllFiles();
-        log.info("files:\n{}", files
-                .entrySet()
-                .stream()
-                .map(entry -> String.format("%s", entry.getValue().getPath()))
-                .collect(Collectors.joining("\n"))
-        );
-        files.values().stream().forEach(file -> {
+        retrieveAllFiles();
+        log.info("Win   games:{}", winGames.size());
+        log.info("Lost  games:{}", lostGames.size());
+        log.info("Drawn games:{}", drawGames.size());
+        winGames.values().stream().forEach(file -> {
+            try {
+                trainFile.train(file, statistics);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        lostGames.values().stream().forEach(file -> {
+            try {
+                trainFile.train(file, statistics);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        drawGames.values().stream().forEach(file -> {
             try {
                 trainFile.train(file, statistics);
             } catch (IOException e) {
@@ -49,30 +68,47 @@ public class AbstractFit {
         });
     }
 
-    private Map<Long, File> retrieveAllFiles() {
-        final SortedMap<Long, File> files = new TreeMap<>();
+    private void retrieveAllFiles() {
         this.configFit.getConfigDirs().forEach(configDir -> {
-            File file = new File(configDir.getDirectory());
-            FileFilter filters = pathname -> {
-                log.info("accept({})", pathname);
-                try {
-                    String filename = pathname.toPath().getFileName().toString();
-                    int fileNum = Integer.parseInt(filename);
-                    return pathname.canRead() &&
-                            pathname.isFile() &&
-                            fileNum >= configDir.getStartNumber() &&
-                            fileNum <= configDir.getEndNumber();
-                } catch (NumberFormatException e) {
-                    return false;
-                }
-            };
-            File[] trainFiles = file.listFiles(filters);
+            File[] trainFiles = getFiles(configDir);
             if (trainFiles != null) {
                 Arrays.stream(trainFiles).forEach(file1 -> {
-                    files.put(file1.lastModified(), file1);
+                    try {
+                        TrainGame trainGame = TrainGame.load(file1);
+                        Double value = trainGame.getValue();
+                        if (value == -1.0) {
+                            lostGames.put(file1.lastModified(), file1);
+                        } else if (value == 0.0) {
+                            drawGames.put(file1.lastModified(), file1);
+                        } else if (value == 1.0) {
+                            winGames.put(file1.lastModified(), file1);
+                        } else
+                            throw new RuntimeException("Unidentified game value:" + value);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    } catch (ClassNotFoundException e) {
+                        throw new RuntimeException(e);
+                    }
                 });
             }
         });
-        return files;
+    }
+
+    private static File[] getFiles(ConfigDir configDir) {
+        File file = new File(configDir.getDirectory());
+        FileFilter filters = pathname -> {
+            try {
+                String filename = pathname.toPath().getFileName().toString();
+                int fileNum = Integer.parseInt(filename);
+                return pathname.canRead() &&
+                        pathname.isFile() &&
+                        fileNum >= configDir.getStartNumber() &&
+                        fileNum <= configDir.getEndNumber();
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        };
+        File[] trainFiles = file.listFiles(filters);
+        return trainFiles;
     }
 }
