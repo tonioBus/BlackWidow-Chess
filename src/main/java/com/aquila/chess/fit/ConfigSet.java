f718f9bba39a6132148171d4d83eee7f40fab6df
Anthony Bussani <bussania@gmail.com>
2024-03-23 08:20:45 +0100

    add set of config dirs to get more train possible in the same time

---
+++ src/main/java/com/aquila/chess/fit/ConfigSet.java
@@ -0,0 +1,31 @@
+package com.aquila.chess.fit;
+
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@XmlRootElement(name = "set")
+public class ConfigSet implements Comparable<ConfigSet> {
+
+    @XmlElement(required = true)
+    private List<ConfigDir> configDirs;
+
+    @XmlAttribute(required = true)
+    private int sequence;
+
+    @XmlAttribute(required = true)
+    private boolean enable;
+
+    @Override
+    public int compareTo(ConfigSet configSet) {
+        return this.sequence - configSet.sequence;
+    }
+}
