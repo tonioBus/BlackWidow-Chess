843fcc3203964285824ec742e67d46dbc47218a5
Anthony Bussani <bussania@gmail.com>
2024-04-28 16:05:06 +0200

    add loading and saving in json

---
+++ src/main/java/com/aquila/chess/ConvertTrainFile.java
@@ -0,0 +1,46 @@
+package com.aquila.chess;
+
+import lombok.extern.slf4j.Slf4j;
+import picocli.CommandLine;
+
+import java.io.IOException;
+
+@Slf4j
+@CommandLine.Command(
+        name = "ConvertTrainFile",
+        description = "ConvertTrainFile POJO to JSON train files"
+)
+public class ConvertTrainFile implements Runnable {
+    @CommandLine.Option(names = {"-fg", "--firstGame"})
+    private int firstGame = 0;
+
+    @CommandLine.Option(names = {"-lg", "--lastGame"})
+    private int lastGame = 0;
+
+    private static final String TRAIN_DIR = "train";
+
+    @Override
+    public void run() {
+        for (int num = firstGame; num <= lastGame; num++) {
+            try {
+                log.info("Converting {}", TrainGame.getFileName(TRAIN_DIR, num, TrainGame.MarshallingType.POJO));
+                TrainGame trainGame = TrainGame.load("train", num, TrainGame.MarshallingType.POJO);
+                trainGame.save("train", num, TrainGame.MarshallingType.JSON);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        ;
+    }
+
+    /**
+     * The learning rate was set to 0.2 and dropped to 0.02, 0.002,
+     * and 0.0002 after 100, 300, and 500 thousand steps for chess
+     */
+    public static void main(final String[] args) throws Exception {
+        CommandLine cmd = new CommandLine(new ConvertTrainFile());
+        cmd.execute(args);
+    }
+}
